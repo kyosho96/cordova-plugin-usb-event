b57eb2c162dfb5328270be8cc22eca8b4b569c02
kyosho- <info@kyosho.net>
2019-10-24 01:49:20 +0900

    mofify method name.

--- www/usb-event.js
+++ www/usb-event.js
@@ -23,6 +23,6 @@ SOFTWARE.
  */
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
+exports.registerEventCallback = function (arg0, success, error) {
     exec(success, error, 'UsbEvent', 'registerEventCallback', [arg0]);
 };
