23d4cf56642df7cd0f01024683cfc6ff32027616
kyosho- <info@kyosho.net>
2019-10-24 20:53:35 +0900

    cleaning.

--- src/android/UsbEvent.java
+++ src/android/UsbEvent.java
@@ -17,11 +17,6 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 /**
  * This class echoes a string called from JavaScript.
@@ -32,21 +27,28 @@ public class UsbEvent extends CordovaPlugin {
 
   private static final String ACTION_EVENT_CALLBACK = "registerEventCallback";
 
+  private static final String PROPERTY_EVENT_KEY = "event";
+  private static final String PROPERTY_VID_KEY = "vendorId";
+  private static final String PROPERTY_PID_KEY = "productId";
+
+  private static final String PROPERTY_EVENT_VALUE_CALLBACK = "callbackRegistered";
+  private static final String PROPERTY_EVENT_VALUE_ATTACHED = "attached";
+  private static final String PROPERTY_EVENT_VALUE_DETACHED = "detached";
+
   private CallbackContext eventCallback;
 
   private UsbManager usbManager;
 
-  private Map<String, UsbDevice> currentDeviceList = new HashMap();
-
   @Override
-  public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+  public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
     switch (action) {
       case ACTION_EVENT_CALLBACK:
         this.registerEventCallback(args, callbackContext);
         return true;
       default:
-        return false;
+        callbackContext.error(String.format("Unsupported action. (action=%s)", action));
     }
+    return false;
   }
 
   private void registerEventCallback(final JSONArray args, final CallbackContext callbackContext) {
@@ -54,21 +56,18 @@ public class UsbEvent extends CordovaPlugin {
     this.registerUsbAttached();
     this.registerUsbDetached();
 
-    cordova.getThreadPool().execute(new Runnable() {
-      public void run() {
-        try {
-          Log.d(TAG, "Registering Event Callback");
-          eventCallback = callbackContext;
-          JSONObject returnObj = new JSONObject();
-          returnObj.put(ACTION_EVENT_CALLBACK, true);
-          // Keep the callback
-          PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, returnObj);
-          pluginResult.setKeepCallback(true);
-          callbackContext.sendPluginResult(pluginResult);
-        } catch (JSONException e) {
-          // TODO:
-          e.printStackTrace();
-        }
+    cordova.getThreadPool().execute(() -> {
+      try {
+        Log.d(TAG, "Registering Event Callback");
+        eventCallback = callbackContext;
+        JSONObject returnObj = new JSONObject();
+        returnObj.put(PROPERTY_EVENT_KEY, PROPERTY_EVENT_VALUE_CALLBACK);
+        // Keep the callback
+        PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, returnObj);
+        pluginResult.setKeepCallback(true);
+        callbackContext.sendPluginResult(pluginResult);
+      } catch (JSONException e) {
+        callbackContext.error(e.getMessage());
       }
     });
   }
@@ -84,9 +83,7 @@ public class UsbEvent extends CordovaPlugin {
   }
 
   private void updateCurrentUsbDeviceList() {
-    this.currentDeviceList.clear();
     HashMap<String, UsbDevice> deviceList = this.usbManager.getDeviceList();
-    this.currentDeviceList.putAll(deviceList);
   }
 
   @Override
@@ -113,58 +110,50 @@ public class UsbEvent extends CordovaPlugin {
     this.cordova.getActivity().unregisterReceiver(this.usbAttachReceiver);
   }
 
-  private static final String PROPERTY_EVENT_KEY = "event";
-  private static final String PROPERTY_EVENT_VALUE_ATTACHED = "attached";
-  private static final String PROPERTY_EVENT_VALUE_DETACHED = "detached";
-
-  BroadcastReceiver usbAttachReceiver = new BroadcastReceiver() {
+  private BroadcastReceiver usbAttachReceiver = new BroadcastReceiver() {
     public void onReceive(Context context, Intent intent) {
       try {
         String action = intent.getAction();
         UsbDevice device = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
 
         if (UsbManager.ACTION_USB_DEVICE_ATTACHED.equals(action) &&
-          UsbEvent.this.eventCallback != null &&
-          device != null) {
+          UsbEvent.this.eventCallback != null && device != null) {
 
           JSONObject returnObj = new JSONObject();
           returnObj.put(PROPERTY_EVENT_KEY, PROPERTY_EVENT_VALUE_ATTACHED);
-          returnObj.put("vendorId", device.getVendorId());
-          returnObj.put("productId", device.getProductId());
+          returnObj.put(PROPERTY_VID_KEY, device.getVendorId());
+          returnObj.put(PROPERTY_PID_KEY, device.getProductId());
 
           PluginResult result = new PluginResult(PluginResult.Status.OK, returnObj);
           result.setKeepCallback(true);
           eventCallback.sendPluginResult(result);
         }
       } catch (JSONException e) {
-        // TODO:
-        e.printStackTrace();
+        eventCallback.error(e.getMessage());
       }
     }
   };
 
-  BroadcastReceiver usbDetachReceiver = new BroadcastReceiver() {
+  private BroadcastReceiver usbDetachReceiver = new BroadcastReceiver() {
     public void onReceive(Context context, Intent intent) {
       try {
         String action = intent.getAction();
         UsbDevice device = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
 
         if (UsbManager.ACTION_USB_DEVICE_DETACHED.equals(action) &&
-          UsbEvent.this.eventCallback != null &&
-          device != null) {
+          UsbEvent.this.eventCallback != null && device != null) {
 
           JSONObject returnObj = new JSONObject();
           returnObj.put(PROPERTY_EVENT_KEY, PROPERTY_EVENT_VALUE_DETACHED);
-          returnObj.put("vendorId", device.getVendorId());
-          returnObj.put("productId", device.getProductId());
+          returnObj.put(PROPERTY_VID_KEY, device.getVendorId());
+          returnObj.put(PROPERTY_PID_KEY, device.getProductId());
 
           PluginResult result = new PluginResult(PluginResult.Status.OK, returnObj);
           result.setKeepCallback(true);
           eventCallback.sendPluginResult(result);
         }
       } catch (JSONException e) {
-        // TODO:
-        e.printStackTrace();
+        eventCallback.error(e.getMessage());
       }
     }
   };
